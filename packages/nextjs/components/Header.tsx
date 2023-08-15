1a06701587d1947927fea48da20af5926fd89725
wildanvin <danvin.jim@gmail.com>
2023-08-15 15:27:29 -0500

    Add name of the project

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -102,8 +102,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-eth</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Compete Learn</span>
+            {/* <span className="text-xs">Recompensa el aprendizaje</span> */}
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">{navLinks}</ul>
