4eb94d30b5b4135c0dfb9b0326b6046d87ac1b18
wildanvin <danvin.jim@gmail.com>
2023-08-16 09:12:26 -0500

    Very scaffold of erc-20

---
+++ packages/hardhat/contracts/testLearnERC20.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract testLearnERC20{
+
+	mapping(address => bool) public approvedByTeacher;
+	address public admin;
+
+    bool public classFinished;
+
+    constructor (address _admin) {
+        
+
+    }
+
+    // immutable USDC
+
+
+    //function to redeem LEARN to the prize pool only if the admin/teacher 
+    // marks the class as finished
+
+    //function to approve an array of students
+    
+
+    //function to set tha class finished
+    // here we save in a variable the total supply of LEARN and will distribute accordingly
+
+    // function that only the admin can claim any tokens
+
+}
