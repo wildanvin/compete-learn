183cbf68c512a6aed534db9c7b53737c46e1a937
wildanvin <danvin.jim@gmail.com>
2023-08-16 17:12:17 -0500

    Improve for mobile

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -14,57 +14,65 @@ const Home: NextPage = () => {
             <span className="block text-4xl font-bold">Compete Learn</span>
             <span className="block text-2xl mb-2">Desarrollo de competencias digitales</span>
           </h1>
-          <p className="text-center text-lg w-1/2">
-            Te damos la bienvenida a CompeteLearn, nuestro programa educativo para la formación docente en el desarrollo
-            de competencias digitales en cinco áreas fundamentales:{" "}
-            <b>
-              búsqueda y gestión de información y datos, comunicación y colaboración, creación de contenidos digitales,
-              seguridad y resolución de problemas
-            </b>
-            .
-          </p>
-          <p className="text-center text-lg w-1/2">
-            Este programa está especialmente diseñado para profesionales de la Educación o que se dediquen a la
-            enseñanza en diferentes niveles y modalidades, a fin de que puedan integrar la tecnología en su práctica
-            educativa y ofrecer a sus estudiantes interesantes experiencias de aprendizaje significativo.
-          </p>
-          <p className="text-center text-lg w-1/2">
-            Para que tu recorrido por el programa sea más ameno y motivante, hemos creado un sistema de recompensas y
-            premiaciones con insignias, tokens y NFT’s que podrás reclamar a medida que vayas avanzando en tu formación,
-            cumpliendo los desafíos de cada módulo y, sobre todo, participando en las actividades síncronas (on line)
-            que tendremos cada semana.
-          </p>
-          <p className="text-center text-lg w-1/2">
-            Hemos distribuido el contenido en cinco (05) módulos, siendo la duración del programa de 10 semanas en las
-            que deberás dedicar al menos 5 horas semanales para la revisión del material y el desarrollo de los desafíos
-            de cada módulo. En total, dedicarás alrededor de 50 horas de formación.
-          </p>
-          <p className="text-center text-lg w-1/2">
-            Al culminar los módulos, obtendrás un NFT que validará tu aprendizaje y, al final de este emocionante
-            recorrido, recibirás un certificado respaldado en la blockchain, garantizando la credibilidad del mismo y
-            ofreciéndote una certificación digital de gran valor en el mundo laboral actual
-          </p>
-          <p className="text-center text-lg w-1/2">
-            Te invitamos a comenzar el programa de formación revisando la sección Información General, donde encontrarás
-            los materiales del programa: objetivos de aprendizaje, criterios de evaluación, estructura del curso,
-            conceptos clave, sistema de recompensa y tutoriales para puedas acceder a los premios.
-          </p>
-          <p className="text-center text-lg w-1/2">
-            Nuestro deseo es que tengas una experiencia de aprendizaje dinámica, motivante y divertida. Porque, ¿Quién
-            dijo que no podemos divertirnos mientras aprendemos?
-          </p>
-          <p className="text-center text-lg w-1/2">¡Comencemos!</p>
+          <div className="text-left text-lg w-full sm:w-1/2">
+            <p>
+              Te damos la bienvenida a CompeteLearn, nuestro programa educativo para la formación docente en el
+              desarrollo de competencias digitales en cinco áreas fundamentales:{" "}
+              <b>
+                búsqueda y gestión de información y datos, comunicación y colaboración, creación de contenidos
+                digitales, seguridad y resolución de problemas
+              </b>
+              .
+            </p>
+            <p>
+              Este programa está especialmente diseñado para profesionales de la Educación o que se dediquen a la
+              enseñanza en diferentes niveles y modalidades, a fin de que puedan integrar la tecnología en su práctica
+              educativa y ofrecer a sus estudiantes interesantes experiencias de aprendizaje significativo.
+            </p>
+            <p>
+              Para que tu recorrido por el programa sea más ameno y motivante, hemos creado un sistema de recompensas y
+              premiaciones con insignias, tokens y NFT’s que podrás reclamar a medida que vayas avanzando en tu
+              formación, cumpliendo los desafíos de cada módulo y, sobre todo, participando en las actividades síncronas
+              (on line) que tendremos cada semana.
+            </p>
+            <p>
+              Hemos distribuido el contenido en cinco (05) módulos, siendo la duración del programa de 10 semanas en las
+              que deberás dedicar al menos 5 horas semanales para la revisión del material y el desarrollo de los
+              desafíos de cada módulo. En total, dedicarás alrededor de 50 horas de formación.
+            </p>
+            <p>
+              Al culminar los módulos, obtendrás un NFT que validará tu aprendizaje y, al final de este emocionante
+              recorrido, recibirás un certificado respaldado en la blockchain, garantizando la credibilidad del mismo y
+              ofreciéndote una certificación digital de gran valor en el mundo laboral actual
+            </p>
+            <p>
+              Te invitamos a comenzar el programa de formación revisando la sección Información General, donde
+              encontrarás los materiales del programa: objetivos de aprendizaje, criterios de evaluación, estructura del
+              curso, conceptos clave, sistema de recompensa y tutoriales para puedas acceder a los premios.
+            </p>
+            <p>
+              Nuestro deseo es que tengas una experiencia de aprendizaje dinámica, motivante y divertida. Porque, ¿Quién
+              dijo que no podemos divertirnos mientras aprendemos?
+            </p>
+            <p className="text-center">¡Comencemos!</p>
+          </div>
         </div>
 
-        <br />
         <br />
         <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring focus:ring-blue-300">
           Inscribirse
         </button>
 
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+
         {/* <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-left items-center max-w-xs rounded-3xl">
               <BugAntIcon className="h-8 w-8 fill-secondary" />
               <p>
                 Tinker with your smart contract using the{" "}
@@ -74,7 +82,7 @@ const Home: NextPage = () => {
                 tab.
               </p>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-left items-center max-w-xs rounded-3xl">
               <SparklesIcon className="h-8 w-8 fill-secondary" />
               <p>
                 Experiment with{" "}
@@ -84,7 +92,7 @@ const Home: NextPage = () => {
                 to build your own UI.
               </p>
             </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
+            <div className="flex flex-col bg-base-100 px-10 py-10 text-left items-center max-w-xs rounded-3xl">
               <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
               <p>
                 Explore your local transactions with the{" "}
--- packages/nextjs/pages/module0.tsx
+++ packages/nextjs/pages/module0.tsx
@@ -12,22 +12,24 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2 font-bold">Módulo 0: Información General</span>
           </h1>
 
-          <p className="text-left text-lg w-1/2">
-            Antes de comenzar tu recorrido por los diferentes módulos del programa, es importante que te conozcas
-            algunos aspectos generales que te ayudarán a vivir una experiencia de aprendizaje más placentera. Te
-            invitamos a leer el siguiente Material:
-          </p>
-          <br />
+          <div className="text-left text-lg w-full sm:w-1/2">
+            <p>
+              Antes de comenzar tu recorrido por los diferentes módulos del programa, es importante que te conozcas
+              algunos aspectos generales que te ayudarán a vivir una experiencia de aprendizaje más placentera. Te
+              invitamos a leer el siguiente Material:
+            </p>
+            <br />
 
-          <div className="text-left text-lg w-1/2">
-            <ol className="list-decimal pl-6">
-              <li className="mb-2 link">Objetivos de aprendizaje</li>
-              <li className="mb-2 link">Criterios de evaluación</li>
-              <li className="mb-2 link">Estructura del curso</li>
-              <li className="mb-2 link">Conceptos clave</li>
-              <li className="mb-2 link">Sistema de recompensas </li>
-              <li className="mb-2 link">Tutoriales para acceder a los premios</li>
-            </ol>
+            <div>
+              <ol className="list-decimal pl-6">
+                <li className="mb-2 link">Objetivos de aprendizaje</li>
+                <li className="mb-2 link">Criterios de evaluación</li>
+                <li className="mb-2 link">Estructura del curso</li>
+                <li className="mb-2 link">Conceptos clave</li>
+                <li className="mb-2 link">Sistema de recompensas </li>
+                <li className="mb-2 link">Tutoriales para acceder a los premios</li>
+              </ol>
+            </div>
           </div>
         </div>
       </div>
