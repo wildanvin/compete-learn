38c69df051d4aef0d3c627e2c7ebc72bbf4ad2dc
wildanvin <danvin.jim@gmail.com>
2023-08-16 09:46:15 -0500

    Basic scaffold teacher FE

---
+++ packages/nextjs/components/ApproveSBT.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+export const ApproveSBT: React.FC = () => {
+  const [inputValues, setInputValues] = useState<string[]>(["", "", "", "", ""]);
+
+  const handleInputChange = (index: number, value: string) => {
+    const updatedValues = [...inputValues];
+    updatedValues[index] = value;
+    setInputValues(updatedValues);
+  };
+
+  const handleSubmit = () => {
+    // Handle the submission of inputValues here
+    console.log("Submitted values:", inputValues);
+  };
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Text Input Form</h1>
+      {inputValues.map((value, index) => (
+        <input
+          key={index}
+          type="text"
+          value={value}
+          onChange={e => handleInputChange(index, e.target.value)}
+          className="w-full mb-2 p-2 border border-gray-300 rounded"
+          placeholder={`Input ${index + 1}`}
+        />
+      ))}
+      <button onClick={handleSubmit} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        Submit
+      </button>
+    </div>
+  );
+};
--- packages/nextjs/pages/teacher.tsx
+++ packages/nextjs/pages/teacher.tsx
@@ -1,5 +1,8 @@
 //import Link from "next/link";
 import type { NextPage } from "next";
+import { ApproveERC20 } from "~~/components/ApproveERC20";
+import { ApproveNFT } from "~~/components/ApproveNFT";
+import { ApproveSBT } from "~~/components/ApproveSBT";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
@@ -7,21 +10,9 @@ const Home: NextPage = () => {
     <>
       <MetaHeader />
       <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5 flex flex-col justify-center items-center">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Profesor</span>
-          </h1>
-          <p className="text-center text-lg w-1/2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Eu tincidunt tortor aliquam nulla. Adipiscing elit ut aliquam purus sit amet luctus
-          </p>
-        </div>
-
-        <br />
-        <br />
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg shadow-md focus:outline-none focus:ring focus:ring-blue-300">
-          Inscribirse
-        </button>
+        <ApproveERC20 />
+        <ApproveNFT />
+        <ApproveSBT />
       </div>
     </>
   );
